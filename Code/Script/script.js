413faaac664fdc444dc829d80f6d1065e27c0567
erwush <fachry0107.mc@gmail.com>
2025-10-12 00:44:10 +0700

    Home Page
    Header, Sidebar, Navbar added but unfinished

--- Code/Script/script.js
+++ Code/Script/script.js
@@ -11,3 +11,27 @@ selector.addEventListener("change", () => {
 const savedTheme = localStorage.getItem("theme") || "cyrene";
 document.documentElement.setAttribute("data-theme", savedTheme);
 selector.value = savedTheme;
+
+
+
+const menuButton = document.getElementById("menuButton");
+const sideBar = document.getElementById("sidebar");
+menuButton.addEventListener("click", () => {
+  menuButton.classList.toggle("active");
+  sideBar.classList.toggle("active");
+});
+
+const headerImg = document.querySelectorAll(".headerInImg");
+
+headerImg.forEach((img) => {
+  const normalSrc = img.getAttribute("src");
+  const hoverSrc = img.getAttribute("data-hover");
+
+  img.addEventListener("mouseover", () => {
+    img.setAttribute("src", hoverSrc);
+  });
+
+  img.addEventListener("mouseout", () => {
+    img.setAttribute("src", normalSrc);
+  });
+});
