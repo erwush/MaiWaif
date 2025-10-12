92a24b2b151ea255c17ec87195325ccabbd5aaed
erwush <fachry0107.mc@gmail.com>
2025-10-12 13:31:48 +0700

    More Feature
    Added custom cursor and added setting section for sidebar

--- Code/Script/script.js
+++ Code/Script/script.js
@@ -12,13 +12,20 @@ const savedTheme = localStorage.getItem("theme") || "cyrene";
 document.documentElement.setAttribute("data-theme", savedTheme);
 selector.value = savedTheme;
 
+const settingBtn = document.getElementById("settingBtn");
 
+const gearIcon = document.getElementById('gearIcon');
+const settingBar = document.getElementById("settingBar");
 
 const menuButton = document.getElementById("menuButton");
 const sideBar = document.getElementById("sidebar");
 menuButton.addEventListener("click", () => {
   menuButton.classList.toggle("active");
   sideBar.classList.toggle("active");
+  if(settingBar.classList.contains("active")){
+    settingBar.classList.toggle("active");
+  }
+  
 });
 
 const headerImg = document.querySelectorAll(".headerInImg");
@@ -34,4 +41,30 @@ headerImg.forEach((img) => {
   img.addEventListener("mouseout", () => {
     img.setAttribute("src", normalSrc);
   });
+});
+
+
+// const settingsBtn = document.getElementById('settingsBtn');
+// const gearIcon = document.getElementById('gearIcon');
+
+// settingsBtn.addEventListener('click', () => {
+//   sidebar.classList.toggle('expanded');
+//   gearIcon.classList.add('rotating');
+//   setTimeout(() => gearIcon.classList.remove('rotating'), 600);
+// });
+
+
+// settingBtn.addEventListener('click', () => {
+//     settingBar.classList.toggle('active');
+//   if (gearIcon.classList.contains('rotating')) return; 
+//   gearIcon.classList.add('rotating');
+//   setTimeout(() => gearIcon.classList.remove('rotating'), 600);
+// });
+
+let rotated = false;
+
+settingBtn.addEventListener('click', () => {
+  settingBar.classList.toggle('active');
+  rotated = !rotated;
+  gearIcon.style.transform = rotated ? "rotate(360deg)" : "rotate(0deg)";
 });
